6fa933e17cc7487bb69a9ce48fb1b3938fa7c572
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-09-12 12:54:28 -0600

    Update Button.js and add DemoOutput
    - Experimenting with child component re evaluation
    
    - Preventing re evaluation with React.memo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
   console.log("App running");
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
